3faf71647590200ce6f35781805d3588d1ada7cf
Josh York <york.josh@gmail.com>
2019-04-06 21:55:10 -0400

    working on answers too

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const AnswerDisplay = ({answer}) => (
-  <div>
-    {answer ? <div>Answer stuff</div>
-    :<div>Display Answer...</div>
-    }
-  </div>
-)
-
-const AnswersList = ({answers}) => (
-  
-  <div>
-    {answers ? (
-      answers.map(answer => <AnswerDisplay answer={answer} />)
-    ) : (
-      <div>...Loading</div>
-    )}
-  </div>
+const AnswerDisplay = ({ answer }) => (
+  <div>{answer ? <div>Answer stuff</div> : <div>Display Answer...</div>}</div>
 );
+
+const AnswersList = ({ answers }) => {
+  console.log("answers: ", answers);
+
+  return (
+    <div>
+      {answers ? (
+        answers.map(answer => <AnswerDisplay answer={answer} />)
+      ) : (
+        <div>...Loading</div>
+      )}
+    </div>
+  );
+};
 /**
  * Get the list of questions from the application's state
  * It is populated by a ../sagas/fetch-answer(s)-saga.
