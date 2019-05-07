7c4813315606fb05be66e26c6bde8a5475d57ff6
Josh York <york.josh@gmail.com>
2019-05-07 19:19:59 -0400

    fix #9

--- data/api-real-url.js
+++ data/api-real-url.js
@@ -1,24 +1,31 @@
 /**
+ * The stackexchange api key
+ *
+ */
+const API_KEY = '3)vQIxQvPHLrC7xt)ZTLvQ((';
+/**
  * The URL to receive a list of questions in JSON from StackOverflow.
  * Works if you paste it into your browser's URL bar.
  * Subject to eventual deprecation by its authors (Use mock data after that point)
  */
-export const questions = `https://api.stackexchange.com/2.0/questions?site=stackoverflow`;
+export const questions = `https://api.stackexchange.com/2.0/questions?site=stackoverflow&key=${API_KEY}`;
 /**
  * The URL to receive details on a single question.
  * This request also returns the body of the question
  * @param id
  * The question ID to fetch
  */
-export const question = (id) => `https://api.stackexchange.com/2.0/questions/${id}?site=stackoverflow&filter=withbody`;
-
+export const question = id =>
+  `https://api.stackexchange.com/2.0/questions/${id}?site=stackoverflow&filter=withbody&key=${API_KEY}`;
 
 /**
  * The URL to receive a list of questions in JSON from StackOverflow with a given tag.
  * Works if you paste it into your browser's URL bar.
  * Subject to eventual deprecation by its authors (Use mock data after that point)
- * @param tag 
+ * @param tag
  */
-export const tagQuestions = (tag) => `https://api.stackexchange.com/2.2/search/advanced?tagged=${tag}&site=stackoverflow`;
+export const tagQuestions = tag =>
+  `https://api.stackexchange.com/2.2/search/advanced?tagged=${tag}&site=stackoverflow&key=${API_KEY}`;
 
-export const answers = (question_id) => `https://api.stackexchange.com/2.2/questions/${question_id}/answers?site=stackoverflow&filter=withbody`;
+export const answers = question_id =>
+  `https://api.stackexchange.com/2.2/questions/${question_id}/answers?site=stackoverflow&filter=withbody&key=${API_KEY}`;
